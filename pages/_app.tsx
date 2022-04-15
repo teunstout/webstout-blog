76daf4d67c962d1c03bc1433f1ce2c8ea273cb1b
teunstout <teunstout@hotmail.com>
2022-04-15 10:35:55 +0200

    Remove secrets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,14 +6,7 @@ import { initializeApp } from "firebase/app";
 import store from "../redux/store";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB4-1AfpcZ5wo2jk9wA-tdhzc49x8MQMHg",
-    authDomain: "webstout-blog.firebaseapp.com",
-    databaseURL: "https://webstout-blog-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "webstout-blog",
-    storageBucket: "gs://webstout-blog.appspot.com/",
-    messagingSenderId: "621119557656",
-    appId: "1:621119557656:web:8d5ce468941a14957f736d",
-    measurementId: "G-RMRV5BSGWT",
+
 };
 
 function MyApp({ Component, pageProps }: AppProps) {
